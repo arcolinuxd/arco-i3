a38b301fee97c5bca8a22fcd0af61316e354c29f
Erik Dubois <erik.dubois@gmail.com>
2018-03-11 15:44:54 +0100

    splitting isoskel

--- 600-software-from-ArcoLinux-repo-v1.sh
+++ 600-software-from-ArcoLinux-repo-v1.sh
@@ -16,22 +16,32 @@ set -e
 echo "ArcoLinux Repository"
 
 sudo pacman -S arcolinux-arc-themes-git --noconfirm --needed
+#sudo pacman -S arcolinux-awesome-git --noconfirm --needed
 #sudo pacman -S arcolinux-common-git --noconfirm --needed
+sudo pacman -S arcolinux-config-git --noconfirm --needed
 #sudo pacman -S arcolinux-conky-collection-git --noconfirm --needed
 #sudo pacman -S arcolinux-docs-git --noconfirm --needed
+#sudo pacman -S arcolinux-geany-git --noconfirm --needed
 sudo pacman -S arcolinux-i3wm-git --noconfirm --needed
-sudo pacman -S arcolinux-iso-skel-git --noconfirm --needed
+#sudo pacman -S arcolinux-local-git --noconfirm --needed
+sudo pacman -S arcolinux-neofetch-git --noconfirm --needed
+sudo pacman -S arcolinux-nitrogen-git --noconfirm --needed
 #sudo pacman -S arcolinux-oblogout-themes-git --noconfirm --needed
 #sudo pacman -S arcolinux-obmenu-generator-git --noconfirm --needed
 #sudo pacman -S arcolinux-openbox-configs-git --noconfirm --needed
 #sudo pacman -S arcolinux-openbox-themes-git --noconfirm --needed
 #sudo pacman -S arcolinux-pipemenus-git --noconfirm --needed
+#sudo pacman -S arcolinux-plank-git --noconfirm --needed
 #sudo pacman -S arcolinux-plank-themes-git --noconfirm --needed
 sudo pacman -S arcolinux-polybar-git --noconfirm --needed
+sudo pacman -S arcolinux-rofi-git --noconfirm --needed
+#sudo pacman -S arcolinux-rofi-themes-git --noconfirm --needed
 sudo pacman -S arcolinux-root-git --noconfirm --needed
 sudo pacman -S arcolinux-slimlock-themes-git --noconfirm --needed
 sudo pacman -S arcolinux-termite-themes-git --noconfirm --needed
+#sudo pacman -S arcolinux-tint2-git --noconfirm --needed
 #sudo pacman -S arcolinux-tint2-themes-git --noconfirm --needed
+sudo pacman -S arcolinux-variety-git --noconfirm --needed
 sudo pacman -S arcolinux-wallpapers-git --noconfirm --needed
 sudo pacman -S arcolinux-xfce-thunar-git --noconfirm --needed
 
@@ -63,5 +73,3 @@ echo "################################################################"
 echo
 rm -rf ~/.local/share/applications
 rm -rf ~/.local/share/xfpanel-switch
-
-
