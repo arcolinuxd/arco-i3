dd5f24cbcbd4ebaafd3605ba09e9413acbfa2fd5
Erik Dubois <erik.dubois@gmail.com>
2018-05-03 09:29:28 +0200

    script 300 for virtualbox

--- 300-software-AUR-repo-v1.sh
+++ 300-software-AUR-repo-v1.sh
@@ -85,7 +85,7 @@ sh AUR/install-surfn-icons-git-v*.sh
 #sh AUR/install-tamzen-font-git-v*.sh
 sh AUR/install-ttf-font-awesome-v*.sh
 sh AUR/install-ttf-mac-fonts-v*.sh
-sh AUR/install-virtualbox-for-linux-kernel-v*.sh
+sh AUR/install-virtualbox-for-linux-v*.sh
 sh AUR/install-xcursor-breeze-v*.sh
 
 #Other possibilities
