5022e938f307044f22dc99e862664b1326789c33
Erik Dubois <erik.dubois@gmail.com>
2018-09-04 14:42:49 +0200

    make sure there is a gpg folder

--- Personal/500-add-keyservers-for-key-importing-v1.sh
+++ Personal/500-add-keyservers-for-key-importing-v1.sh
@@ -12,6 +12,7 @@ set -e
 #   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
 #
 ##################################################################################################################
+[ -d $HOME"/.gnupg" ] || mkdir -p $HOME"/.gnupg"
 
 echo '
 
