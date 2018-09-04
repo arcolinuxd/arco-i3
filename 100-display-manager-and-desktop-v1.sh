a989294faa99a0feb61681abb04ba0ba37ad5085
Erik Dubois <erik.dubois@gmail.com>
2018-09-04 21:50:11 +0200

    more info in script 100

--- 100-display-manager-and-desktop-v1.sh
+++ 100-display-manager-and-desktop-v1.sh
@@ -18,10 +18,14 @@ echo "Stop the installation with CTRL + C"
 echo "and run the alias - mirror in the terminal"
 echo "#################################################"
 
-sudo pacman -Syyu
-sudo pacman -S lightdm --noconfirm --needed
+sudo pacman -Syyu --noconfirm
+#installing displaymanager or login manager
+sudo pacman -S --noconfirm --needed lightdm
+sudo pacman -S --noconfirm --needed arcolinux-lightdm-gtk-greeter arcolinux-lightdm-gtk-greeter-settings
+#installing desktop environment
 sudo pacman -S --noconfirm --needed i3status
 sh AUR-DS/install-i3-gaps-next-*.sh
+#enabling displaymanager or login manager
 sudo systemctl enable lightdm.service -f
 sudo systemctl set-default graphical.target
 
