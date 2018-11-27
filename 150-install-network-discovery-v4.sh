354c1fa28d7ac7b60d15f886d4bea23ef9fb9f7b
Erik Dubois <erik.dubois@gmail.com>
2018-11-27 17:34:22 +0100

    improvement of 150 - nsswitch.conf for arcolinux arcolinuxd and arcolinuxb

--- 150-install-network-discovery-v4.sh
+++ 150-install-network-discovery-v4.sh
@@ -21,8 +21,17 @@ sudo systemctl start avahi-daemon.service
 
 #shares on a mac
 sudo pacman -S --noconfirm --needed nss-mdns
-sudo sed -i 's/dns/mdns dns wins/g' /etc/nsswitch.conf
 
+#change nsswitch.conf for access to nas servers
+#original line comes from the package filesystem
+#hosts: files mymachines myhostname resolve [!UNAVAIL=return] dns
+#ArcoLinux line
+#hosts: files mymachines resolve [!UNAVAIL=return] mdns dns wins myhostname
+
+#first part
+sudo sed -i 's/files mymachines myhostname/files mymachines/g' /etc/nsswitch.conf
+#last part
+sudo sed -i 's/\[\!UNAVAIL=return\] dns/\[\!UNAVAIL=return\] mdns dns wins myhostname/g' /etc/nsswitch.conf
 echo "################################################################"
 echo "####       network discovery  software installed        ########"
 echo "################################################################"
