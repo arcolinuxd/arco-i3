de73279c4b1b8104075868227c7776b66bedfeae
Erik Dubois <erik.dubois@gmail.com>
2018-12-31 07:49:20 +0100

    new scripts autologin arch linux

--- 800-autologin-v3.sh
+++ 800-autologin-v3.sh
@@ -6,6 +6,8 @@ set -e
 # Website	:	https://www.arcolinux.info
 # Website	:	https://www.arcolinux.com
 # Website	:	https://www.arcolinuxd.com
+# Website	:	https://www.arcolinuxb.com
+# Website	:	https://www.arcolinuxiso.com
 # Website	:	https://www.arcolinuxforum.com
 ##################################################################################################################
 #
@@ -13,12 +15,26 @@ set -e
 #
 ##################################################################################################################
 
-echo "THIS SCRIPT WILL ASK YOUR LOGINNAME "
-echo "AND WILL USE IT TO ADD YOU TO THE AUTOLOGIN GROUP "
-echo "https://wiki.archlinux.org/index.php/LightDM#Enabling_autologin"
+echo "This script will ask your login name "
+echo "It will be used to add you to the group autologin "
+#echo "https://wiki.archlinux.org/index.php/LightDM#Enabling_autologin"
+
+#checking if group autologin exists for Arch Linux
+function grpexists {
+if [ $(getent group $1) ]; then
+  echo "Check : Group $1 exists"
+else
+  echo "Group $1 does not exist. Go to ArchWay folder"
+  echo "Run the script to create the group autologin and"
+  echo "rerun this script afterwards."
+  exit 1
+fi
+}
+grpexists autologin
 
 ##Change your username here
-read -p "What is your login? It will be used to add this user to the group autologin : " choice
+read -p "What is your login?
+It will be used to add this user to the group autologin : " choice
 sudo gpasswd -a $choice autologin
 
 sudo sed -i 's/'#autologin-user='/'autologin-user=$choice'/g' /etc/lightdm/lightdm.conf
