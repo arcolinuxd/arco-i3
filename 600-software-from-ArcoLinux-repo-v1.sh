71d3e2dad665e2f80ea72968c24886dc27c08de1
Erik Dubois <erik.dubois@gmail.com>
2019-02-03 22:28:37 +0100

    new package for deepin added to the list in 600

--- 600-software-from-ArcoLinux-repo-v1.sh
+++ 600-software-from-ArcoLinux-repo-v1.sh
@@ -27,6 +27,7 @@ sudo pacman -S arcolinux-bin-git --noconfirm --needed
 #sudo pacman -S arcolinux-bspwm-git --noconfirm --needed
 #sudo pacman -S arcolinux-common-git --noconfirm --needed
 #sudo pacman -S arcolinux-config-cinnamon-git --noconfirm --needed
+#sudo pacman -S arcolinux-config-deepin-git --noconfirm --needed
 sudo pacman -S arcolinux-config-git --noconfirm --needed
 #sudo pacman -S arcolinux-config-gnome-git --noconfirm --needed
 #sudo pacman -S arcolinux-config-mate-git --noconfirm --needed
