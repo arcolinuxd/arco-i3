1dbf0ac7bee77d797827624e42c569b9f4c162df
Erik Dubois <erik.dubois@gmail.com>
2019-09-29 18:50:32 +0200

    spotifywm-git out of githubs - only in nemesis

--- 500-software-AUR-repo-distro-specific-v1.sh
+++ 500-software-AUR-repo-distro-specific-v1.sh
@@ -21,8 +21,6 @@ sh AUR-DS/install-i3blocks-v*.sh
 
 sh AUR-DS/install-python-pywal-v*.sh
 
-sh AUR-DS/install-spotifywm-git-v*.sh
-
 sh AUR/install-yad-v*.sh
 
 echo "################################################################"
