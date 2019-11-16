f21810282ed446f244ff3e681c8d2b206024edbc
Erik Dubois <erik.dubois@gmail.com>
2019-11-16 11:31:15 +0100

    oblogout clean out

--- 600-software-from-ArcoLinux-repo-v1.sh
+++ 600-software-from-ArcoLinux-repo-v1.sh
@@ -72,13 +72,8 @@ sudo pacman -S arcolinux-local-xfce4-git --noconfirm --needed
 sudo pacman -S arcolinux-mirrorlist-git --noconfirm --needed
 sudo pacman -S arcolinux-neofetch-git --noconfirm --needed
 sudo pacman -S arcolinux-nitrogen-git --noconfirm --needed
-#sudo pacman -S arcolinux-oblogout-awesome --noconfirm --needed
 #sudo pacman -S arcolinux-oblogout --noconfirm --needed
-#sudo pacman -S arcolinux-oblogout-bspwm --noconfirm --needed
-#sudo pacman -S arcolinux-oblogout-herbstluftwm --noconfirm --needed
-#sudo pacman -S arcolinux-oblogout-qtile --noconfirm --needed
 #sudo pacman -S arcolinux-oblogout-themes-git --noconfirm --needed
-#sudo pacman -S arcolinux-oblogout-xmonad --noconfirm --needed
 #sudo pacman -S arcolinux-obmenu-generator-git --noconfirm --needed
 #sudo pacman -S arcolinux-obmenu-generator-minimal-git --noconfirm --needed
 #sudo pacman -S arcolinux-openbox-git --noconfirm --needed
