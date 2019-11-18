92efb6dcbffbf1b47ddfea07ae974193e072d7db
Erik Dubois <erik.dubois@gmail.com>
2019-11-18 11:36:44 +0100

    update

--- 100-display-manager-and-desktop-v1.sh
+++ 100-display-manager-and-desktop-v1.sh
@@ -26,9 +26,7 @@ sudo pacman -S --noconfirm --needed lightdm
 sudo pacman -S --noconfirm --needed arcolinux-lightdm-gtk-greeter arcolinux-lightdm-gtk-greeter-settings
 sudo pacman -S --noconfirm --needed arcolinux-wallpapers-git
 #installing desktop environment
-if pacman -Qi i3-wm &> /dev/null; then
-		sudo pacman -R i3-wm --noconfirm
-fi
+sudo pacman -R i3-wm --noconfirm
 sudo pacman -S --noconfirm --needed i3status
 sudo pacman -S --noconfirm --needed i3-gaps
 #enabling displaymanager or login manager
