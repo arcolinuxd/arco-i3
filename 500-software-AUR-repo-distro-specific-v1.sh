3cf0c7e5ca91f540bb363a123987f5ce15bcdf60
Erik Dubois <erik.dubois@gmail.com>
2019-12-01 16:21:52 +0100

    more help on the set -e

--- 500-software-AUR-repo-distro-specific-v1.sh
+++ 500-software-AUR-repo-distro-specific-v1.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 set -e
+echo "One or more packages are coming from AUR as source."
+echo "If a package from AUR fails to install, all other packages after that will NOT be installed."
+echo "We put the set -e for you to notice some package broke the script and you can intervene manually."
+echo "Put a hashtag in front of set -e to keep installing (the rest) even if there is an error."
+echo "Then figure out how to get the broken package installed or leave it until the maintainer from AUR"
+echo "will fix it for you. Give it some time."
+sleep 3
 ##################################################################################################################
 # Author	:	Erik Dubois
 # Website	:	https://www.erikdubois.be
