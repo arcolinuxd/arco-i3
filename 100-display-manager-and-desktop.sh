5df9f9e0efe757921dcad0e330dcd80d0e0978e5
Erik Dubois <erik.dubois@gmail.com>
2021-04-25 19:33:34 +0200

    lightdm replaces with sddm

--- 100-display-manager-and-desktop.sh
+++ 100-display-manager-and-desktop.sh
@@ -47,9 +47,7 @@ echo "Installation of the core software"
 ###############################################################################
 
 list=(
-lightdm
-arcolinux-lightdm-gtk-greeter
-arcolinux-lightdm-gtk-greeter-settings
+sddm
 arcolinux-wallpapers-git
 thunar
 thunar-archive-plugin
@@ -86,10 +84,10 @@ cp -Rf ~/.config ~/.config-backup-$(date +%Y.%m.%d-%H.%M.%S)
 cp -arf /etc/skel/. ~
 
 tput setaf 5;echo "################################################################"
-echo "Enabling lightdm as display manager"
+echo "Enabling sddm as display manager"
 echo "################################################################"
 echo;tput sgr0
-sudo systemctl enable lightdm.service -f
+sudo systemctl enable sddm.service -f
 
 tput setaf 7;echo "################################################################"
 echo "You now have a very minimal functional desktop"
