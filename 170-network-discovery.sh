818a56e61c7514a91896dce9075d578a9b8db797
Erik Dubois <erik.dubois@gmail.com>
2022-03-06 09:37:28 +0100

    nsswitch.conf if on Arch Linux

--- 170-network-discovery.sh
+++ 170-network-discovery.sh
@@ -72,11 +72,17 @@ echo;tput sgr0
 
 # https://wiki.archlinux.org/title/Domain_name_resolution
 if [ -f /usr/local/share/arcolinux/nsswitch.conf ]; then
-	echo "Make backup and copy ArcoLinux conf over"
+	echo "Make backup and copy the ArcoLinux nsswitch.conf to /etc/nsswitch.conf"
 	echo
 	sudo cp /etc/nsswitch.conf /etc/nsswitch.conf.bak
 	sudo cp /usr/local/share/arcolinux/nsswitch.conf /etc/nsswitch.conf
+else
+	echo "Getting latest /etc/nsswitch.conf from the internet"
+	sudo cp /etc/nsswitch.conf /etc/nsswitch.conf.bak
+	sudo wget https://raw.githubusercontent.com/arcolinux/arcolinuxl-iso/master/archiso/airootfs/etc/nsswitch.conf -O $workdir/etc/nsswitch.conf
 fi
+
+
 
 tput setaf 5;echo "################################################################"
 echo "Enabling services"
