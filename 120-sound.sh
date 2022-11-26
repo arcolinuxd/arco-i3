847f8ee8e723fb38394eff1a768ea46240ea1d72
arcolinuxz <arcolinuxinfo@gmail.com>
2022-11-26 08:00:00 +0100

    pipewire

--- 120-sound.sh
+++ 120-sound.sh
@@ -42,39 +42,114 @@ func_install() {
     fi
 }
 
-###############################################################################
-echo "Installation of sound software"
-###############################################################################
+function_pulseaudio() {
+	list=(
+	pulseaudio
+	pulseaudio-alsa
+	pavucontrol
+	alsa-firmware
+	alsa-lib
+	alsa-plugins
+	alsa-utils
+	gstreamer
+	gst-plugins-good
+	gst-plugins-bad
+	gst-plugins-base
+	gst-plugins-ugly
+	playerctl
+	volumeicon
+	)
 
-list=(
-pulseaudio
-pulseaudio-alsa
-pavucontrol
-alsa-firmware
-alsa-lib
-alsa-plugins
-alsa-utils
-gstreamer
-gst-plugins-good
-gst-plugins-bad
-gst-plugins-base
-gst-plugins-ugly
-playerctl
-volumeicon
-)
+	count=0
 
-count=0
+	for name in "${list[@]}" ; do
+		count=$[count+1]
+		tput setaf 3;echo "Installing package nr.  "$count " " $name;tput sgr0;
+		func_install $name
+	done
 
-for name in "${list[@]}" ; do
-	count=$[count+1]
-	tput setaf 3;echo "Installing package nr.  "$count " " $name;tput sgr0;
-	func_install $name
-done
+	###############################################################################
 
-###############################################################################
+	tput setaf 11;
+	echo "################################################################"
+	echo "Software has been installed"
+	echo "################################################################"
+	echo;tput sgr0
+}
 
-tput setaf 11;
+function_pipewire() {
+	list=(
+	pipewire
+	pipewire-pulse
+	pipewire-alsa
+	pipewire-jack
+	pipewire-zeroconf
+	pavucontrol
+	alsa-utils
+	alsa-plugins
+	alsa-lib
+	alsa-firmware
+	gstreamer
+	gst-plugins-good
+	gst-plugins-bad
+	gst-plugins-base
+	gst-plugins-ugly   
+	volumeicon
+	playerctl
+	)
+
+	count=0
+
+	for name in "${list[@]}" ; do
+		count=$[count+1]
+		tput setaf 3;echo "Installing package nr.  "$count " " $name;tput sgr0;
+		func_install $name
+	done
+
+	###############################################################################
+
+	tput setaf 11;
+	echo "################################################################"
+	echo "Software has been installed"
+	echo "################################################################"
+	echo;tput sgr0
+}
+
+echo
+tput setaf 2
 echo "################################################################"
-echo "Software has been installed"
+echo "#####  Choose pulseaudio or pipewire to have sound          ####"
 echo "################################################################"
-echo;tput sgr0
+tput sgr0
+echo
+echo "Select the correct number"
+echo
+echo "0.  Do nothing"
+echo "1.  Pulseaudio"
+echo "2.  Pipewire"
+echo "Type the number..."
+
+read CHOICE
+
+case $CHOICE in
+
+    0 )
+		echo
+		echo "########################################"
+		echo "We did nothing as per your request"
+		echo "########################################"
+		echo
+		;;
+
+    1 )
+		function_pulseaudio 
+      	;;
+    2 )
+		function_pipewire
+		;;
+    * )
+		echo "#################################"
+		echo "Choose the correct number"
+		echo "#################################"
+		;;
+esac
