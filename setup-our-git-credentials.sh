b0df9071a3539742bbb3c6174a6dddf653b9dda4
Erik Dubois <erik.dubois@gmail.com>
2022-12-27 10:31:20 +0100

    setup

--- setup-our-git-credentials.sh
+++ setup-our-git-credentials.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 #set -e
 ##################################################################################################################
-# Author 	: Erik Dubois
+# Author    : Erik Dubois
 # Website   : https://www.erikdubois.be
 # Website   : https://www.alci.online
-# Website	: https://www.arcolinux.info
-# Website	: https://www.arcolinux.com
-# Website	: https://www.arcolinuxd.com
-# Website	: https://www.arcolinuxb.com
-# Website	: https://www.arcolinuxiso.com
-# Website	: https://www.arcolinuxforum.com
+# Website   : https://www.ariser.eu
+# Website   : https://www.arcolinux.info
+# Website   : https://www.arcolinux.com
+# Website   : https://www.arcolinuxd.com
+# Website   : https://www.arcolinuxb.com
+# Website   : https://www.arcolinuxiso.com
+# Website   : https://www.arcolinuxforum.com
 ##################################################################################################################
 #
 #   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
 #
 ##################################################################################################################
-#tput setaf 0 = black 
-#tput setaf 1 = red 
+#tput setaf 0 = black
+#tput setaf 1 = red
 #tput setaf 2 = green
-#tput setaf 3 = yellow 
-#tput setaf 4 = dark blue 
+#tput setaf 3 = yellow
+#tput setaf 4 = dark blue
 #tput setaf 5 = purple
-#tput setaf 6 = cyan 
-#tput setaf 7 = gray 
+#tput setaf 6 = cyan
+#tput setaf 7 = gray
 #tput setaf 8 = light blue
 ##################################################################################################################
 
@@ -54,7 +55,7 @@ echo
 #https://blog.nillsf.com/index.php/2021/05/27/github-sso-using-password-protected-ssh-keys
 
 project=$(basename `pwd`)
-githubdir=$(basename $(dirname "$PWD"))
+githubdir=$(basename $(dirname "$PWD" | tr '[:upper:]' '[:lower:]'))
 echo "-----------------------------------------------------------------------------"
 echo "this is project https://github.com/$githubdir/$project"
 echo "-----------------------------------------------------------------------------"
@@ -64,6 +65,9 @@ git config --global push.default simple
 git config --global user.name "arcolinuxz"
 git config --global user.email "arcolinuxinfo@gmail.com"
 sudo git config --system core.editor nano
+#git config --global credential.helper cache
+#git config --global credential.helper 'cache --timeout=32000'
+
 git remote set-url origin git@github.com-arc:$githubdir/$project
 
 echo
